edb00792dbd83d3c135e36293d9ca2537cddc47c
Louis Tsang <louistsang5@gmail.com>
2022-01-13 20:39:51 +0800

    Change login function to also obtain current user from pipe

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, throwError } from "rxjs";
-import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from "rxjs";
+import { map } from 'rxjs/operators';
 import { User } from "../_user/user.model";
 import { environment } from "src/environments/environment";
 import { AccessToken } from "../_user/accessToken.model";
@@ -9,11 +9,11 @@ import { AccessToken } from "../_user/accessToken.model";
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
     private currentUserSubject: BehaviorSubject<User>;
-    private currentUser: Observable<User>;
+    private currentUserObservable: Observable<User>;
     private currentUserKey = 'currentUser';
 
     private accessTokenSubject: BehaviorSubject<AccessToken>;
-    private accessToken: Observable<AccessToken>;
+    private accessTokenObservable: Observable<AccessToken>;
     private accessTokenKey = 'accessToken';
 
     constructor(
@@ -21,28 +21,30 @@ export class AuthenticationService {
     ) {
         //Initialize current user from local storage
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem(this.currentUserKey) as string));
-        this.currentUser = this.currentUserSubject.asObservable();
+        this.currentUserObservable = this.currentUserSubject.asObservable();
 
         //Initialize access token from local storage
         this.accessTokenSubject = new BehaviorSubject<AccessToken>(JSON.parse(localStorage.getItem(this.accessTokenKey) as string));
-        this.accessToken = this.accessTokenSubject.asObservable();
+        this.accessTokenObservable = this.accessTokenSubject.asObservable();
     }
 
-    public get currentUserValue(): User {
+    public get currentUser(): User {
         return this.currentUserSubject.value;
     }
 
-    public get accessTokenValue(): AccessToken {
+    public get accessToken(): AccessToken {
         return this.accessTokenSubject.value;
     }
 
-    public login(email: string, password: string) {
-        const url = `${environment.apiURL}/user/login`
+    public login(email: string, password: string): Observable<AccessToken> {
+        const url = `${environment.apiURL}/user/login`;
         return this.http.post(url, {email, password})
         .pipe(
+            //Get response and return token
             map(res => {
-                if (!((res as any).token && (res as any).expiryDate))
-                    return res;
+                if (!res || !((res as any).token && (res as any).expiryDate)) {
+                    throw new Error('Unable to obtain access token');
+                }
 
                 const token = {
                     token: (res as any).token,
@@ -53,8 +55,22 @@ export class AuthenticationService {
                 localStorage.setItem(this.accessTokenKey, JSON.stringify(token));
                 this.accessTokenSubject.next(token);
 
-                return res;
+                return token;
             }),
+            //Get user info from token
+            map(token => {
+                const userUrl = `${environment.apiURL}/user/self`;
+                const headers = new HttpHeaders({ 'x-access-token': token.token });
+                this.http.get(userUrl, { headers }).subscribe({
+                    next: (res) => {
+                        const { email, alias, firstName, lastName } = res as any;
+                        const user = { email, alias, firstName, lastName } as User;
+                        localStorage.setItem(this.currentUserKey, JSON.stringify(user));
+                        this.currentUserSubject.next(user);
+                    }
+                })
+                return token;
+            })
         );
     }
 }
