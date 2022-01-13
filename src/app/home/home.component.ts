dc3f9093df18ad1c92cc7ad17afab3490c5eaee6
Louis Tsang <louistsang5@gmail.com>
2022-01-13 21:31:48 +0800

    changed to _model dir

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Token } from '@angular/compiler/src/ml_parser/tokens';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../_services/authentication.service';
-import { User } from '../_user/user.model';
+import { User } from '../_models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -17,8 +17,11 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    if (!(this.authenticaionService.currentUser && this.authenticaionService.accessToken))
-      this.router.navigate(['login']);
+    console.log('home page');
+    if (!(this.authenticaionService.currentUser && this.authenticaionService.accessToken)) {
+      console.log('not logged');
+      this.router.navigate(['login'], { queryParams: { returnUrl: '/' } });
+    }
     console.log('already logged in');
   }
 
