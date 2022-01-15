fc5b709d61b1305d14160ceb4f74d2725dc455cc
Louis Tsang <louistsang5@gmail.com>
2022-01-15 11:45:36 +0800

    Added auth service to app root to not display top bar when no user is found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from './_models/user';
+import { AuthenticationService } from './_services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'post-app-frontend';
+  userSubscription: Subscription;
+  user: User | null;
+
+  constructor(
+    private authenticationService: AuthenticationService
+  ) { }
+
+  ngOnInit(): void {
+    this.userSubscription = this.authenticationService.currentUserObservable.subscribe({
+      next: (user) => {
+        this.user = user;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 }
