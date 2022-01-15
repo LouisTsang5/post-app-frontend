8356371689185a25826c9bc6e313402ba2a74458
Louis Tsang <louistsang5@gmail.com>
2022-01-15 12:02:56 +0800

    Removed HttpClient from app modules dir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
