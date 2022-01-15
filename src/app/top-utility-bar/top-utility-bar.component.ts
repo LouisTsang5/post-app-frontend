8d0b20a468609a7903fa2bcf83fbc645e45b980a
Louis Tsang <louistsang5@gmail.com>
2022-01-15 12:02:22 +0800

    Added logout function to top bar

--- src/app/top-utility-bar/top-utility-bar.component.ts
+++ src/app/top-utility-bar/top-utility-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
   selector: 'app-top-utility-bar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopUtilityBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  public onLogout() {
+    this.authenticationService.logout();
+  }
 }
