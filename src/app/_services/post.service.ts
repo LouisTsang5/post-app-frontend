3ef58990a55b4c94e1dba1f119ccc131c1368c37
Louis Tsang <louistsang5@gmail.com>
2022-01-18 20:43:42 +0800

    Added current user post function

--- src/app/_services/post.service.ts
+++ src/app/_services/post.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Post } from '../_models/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUserPosts(): Observable<Post[]> {
+    const url = `${environment.apiURL}/post`;
+    return this.http.get(url)
+    .pipe(
+      map((res: any) => {
+        const posts: Post[] = [];
+        for (let i = 0; i < res.length; i++) {
+          posts.push({
+            title: res[i].title,
+            content: res[i].content
+          });
+        }
+        return posts;
+      })
+    );
+  }
 }
