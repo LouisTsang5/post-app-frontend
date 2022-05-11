e56a16acca4e60b57df70a4bf5dffa51a6b8cb97
louistsang5 <louistsang5@gmail.com>
2022-05-11 21:43:21 +0800

    added media file display in post page

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Post } from 'src/app/_models/post';
 import { PostService } from 'src/app/_services/post.service';
@@ -12,15 +13,25 @@ export class PostComponent implements OnInit {
 
     private id: string;
     post: Post;
+    mediaUrls: SafeResourceUrl[];
 
     constructor(
         private postService: PostService,
         private route: ActivatedRoute,
+        private sanitization: DomSanitizer,
     ) { }
 
     async ngOnInit(): Promise<void> {
         this.id = this.route.snapshot.queryParams['id'];
         this.post = await this.postService.getPost(this.id);
+        if (this.post.multiMedia) {
+            const urls = await Promise.all(
+                this.post.multiMedia.map(async (media) => {
+                    return await this.postService.getMedia(this.post.id, media.index);
+                })
+            );
+            this.mediaUrls = urls.map((url) => this.sanitization.bypassSecurityTrustResourceUrl(url));
+        }
     }
 
 }
