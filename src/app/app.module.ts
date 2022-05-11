18df0e7c8dc770304d55f37b03c5787abcf1d959
louistsang <louis.tsang@ricoh.com.hk>
2022-05-11 17:21:06 +0800

    renamed directory to register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 import { LoginFormComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TopUtilityBarComponent } from './components/top-utility-bar/top-utility-bar.component';
-import { RegistrationFormComponent } from './pages/registration-form/register.component';
+import { RegistrationFormComponent } from './pages/register/register.component';
 import { PostsPageComponent } from './components/posts/posts-page/posts-page.component';
 import { CreatePostsFormComponent } from './components/posts/create-posts-form/create-posts-form.component';
 import { FileUploadComponent } from './components/utils/file-upload/file-upload.component';
