b9e59cedcaf984f5eb6e02020afd5ea61eaabcfe
louistsang <louis.tsang@ricoh.com.hk>
2022-05-11 17:07:41 +0800

    updated post service to get request url to return url object instead of plain string

--- src/app/_services/post.service.ts
+++ src/app/_services/post.service.ts
@@ -6,77 +6,77 @@ import { Post, PostFormData } from '../_models/post';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PostService {
 
-  private userPostsSubject: BehaviorSubject<Post[]>;
-  public userPostsObservable: Observable<Post[]>;
+    private userPostsSubject: BehaviorSubject<Post[]>;
+    public userPostsObservable: Observable<Post[]>;
 
-  constructor(
-    private http: HttpClient,
-    private authenticationService: AuthenticationService
-  ) { 
-    this.userPostsSubject = new BehaviorSubject<Post[]>([]);
-    this.userPostsObservable = this.userPostsSubject.asObservable();
-  }
+    constructor(
+        private http: HttpClient,
+        private authenticationService: AuthenticationService
+    ) {
+        this.userPostsSubject = new BehaviorSubject<Post[]>([]);
+        this.userPostsObservable = this.userPostsSubject.asObservable();
+    }
 
-  private get requestUrl() {
-    return `${environment.apiURL}/post`;
-  }
+    private get requestUrl() {
+        return new URL(`${environment.apiURL}/post`);
+    }
 
-  private get requestHeader() {
-    if (!this.authenticationService.accessToken)
-      throw new Error('No access token available');
-    return new HttpHeaders({ 'x-access-token': this.authenticationService.accessToken?.token as string });
-  }
+    private get requestHeader() {
+        if (!this.authenticationService.accessToken)
+            throw new Error('No access token available');
+        return new HttpHeaders({ 'x-access-token': this.authenticationService.accessToken?.token as string });
+    }
 
-  getUserPosts() {
-    this.http.get(this.requestUrl, {headers: this.requestHeader})
-    .pipe(
-      first(),
-      map((res: any) => {
-        if (!res)
-          return [];
+    getUserPosts() {
+        this.http.get(this.requestUrl.toString(), { headers: this.requestHeader })
+            .pipe(
+                first(),
+                map((res: any) => {
+                    if (!res)
+                        return [];
 
-        const posts: Post[] = [];
-        for (let i = 0; i < res.length; i++) {
-          const post = res[i] as Post;
-          posts.push(post);
-        }
-        return posts;
-      }),
-      map((posts) => {
-        this.userPostsSubject.next(posts);
-        return posts;
-      })
-    ).subscribe();
-  }
+                    const posts: Post[] = [];
+                    for (let i = 0; i < res.length; i++) {
+                        const post = res[i] as Post;
+                        posts.push(post);
+                    }
+                    return posts;
+                }),
+                map((posts) => {
+                    this.userPostsSubject.next(posts);
+                    return posts;
+                })
+            ).subscribe();
+    }
 
-  createPost(post: PostFormData) {
-    //Transform post form data to form data
-    const formData = new FormData();
-    formData.append('title', post.title);
-    formData.append('content', post.content);
-    if (post.multimedia) post.multimedia.map((file) => formData.append('multimedia', file, file.name));
+    createPost(post: PostFormData) {
+        //Transform post form data to form data
+        const formData = new FormData();
+        formData.append('title', post.title);
+        formData.append('content', post.content);
+        if (post.multimedia) post.multimedia.map((file) => formData.append('multimedia', file, file.name));
 
-    //Post to api
-    this.http.post(this.requestUrl, formData, {headers: this.requestHeader})
-    .pipe(
-      first(),
-      map((res) => {
-        this.getUserPosts();
-      })
-    ).subscribe();
-  }
+        //Post to api
+        this.http.post(this.requestUrl.toString(), formData, { headers: this.requestHeader })
+            .pipe(
+                first(),
+                map((res) => {
+                    this.getUserPosts();
+                })
+            ).subscribe();
+    }
 
-  deletePost(id: string) {
-    const apiUrl = new URL(this.requestUrl);
-    const url = new URL(`${apiUrl.pathname}/${id}`, apiUrl.origin).toString();
-    this.http.delete(url, {headers: this.requestHeader})
-    .pipe(
-      first(),
-      map(() => this.getUserPosts())
-    ).subscribe();
-  }
+    deletePost(id: string) {
+        const apiUrl = new URL(this.requestUrl);
+        const url = new URL(`${apiUrl.pathname}/${id}`, apiUrl.origin).toString();
+        this.http.delete(url, { headers: this.requestHeader })
+            .pipe(
+                first(),
+                map(() => this.getUserPosts())
+            ).subscribe();
+    }
 }
