2c339d765990658b24e19a1015d58c678135e4ec
louistsang <louis.tsang@ricoh.com.hk>
2022-05-13 13:44:18 +0800

    updated media viewer to use logger to log info

--- src/app/components/utils/media-viewer/media-viewer.component.ts
+++ src/app/components/utils/media-viewer/media-viewer.component.ts
@@ -1,35 +1,36 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { LoggerService } from 'src/app/_services/logger.service';
 
 @Component({
-	selector: 'app-media-viewer',
-	templateUrl: './media-viewer.component.html',
-	styleUrls: ['./media-viewer.component.css']
+    selector: 'app-media-viewer',
+    templateUrl: './media-viewer.component.html',
+    styleUrls: ['./media-viewer.component.css']
 })
 export class MediaViewerComponent implements OnInit {
 
-	@Input() srcs?: string[];
+    @Input() srcs?: string[];
     @Input() height: string;
     @Input() width: string;
     safeUrls: SafeUrl[];
     index: number;
 
-	constructor(
+    constructor(
         private sanitization: DomSanitizer,
+        private logger: LoggerService,
     ) { }
 
-	ngOnInit(): void {
+    ngOnInit(): void {
         this.index = 0;
-		if(this.srcs) this.safeUrls = this.srcs.map((url) => this.sanitization.bypassSecurityTrustResourceUrl(url));
-	}
+        if (this.srcs) this.safeUrls = this.srcs.map((url) => this.sanitization.bypassSecurityTrustResourceUrl(url));
+    }
 
     onClickNext() {
         if (this.index < this.safeUrls.length - 1) this.index++;
-        console.log(this.index);
+        this.logger.log(`Image index: ${this.index}`);
     }
-
     onClickPrev() {
         if (this.index > 0) this.index--;
-        console.log(this.index);
+        this.logger.log(`Image index: ${this.index}`);
     }
 }
