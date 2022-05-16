cf3f92fb38b4e957d911142da40d73ffcfb179f3
louistsang5 <louistsang5@gmail.com>
2022-05-16 20:39:50 +0800

    added auto play button to media viewer to toggle on and off

--- src/app/components/utils/media-viewer/media-viewer.component.ts
+++ src/app/components/utils/media-viewer/media-viewer.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { LoggerService } from 'src/app/_services/logger.service';
 
 @Component({
@@ -7,12 +8,16 @@ import { LoggerService } from 'src/app/_services/logger.service';
     templateUrl: './media-viewer.component.html',
     styleUrls: ['./media-viewer.component.css']
 })
-export class MediaViewerComponent implements OnInit {
+export class MediaViewerComponent implements OnInit, OnDestroy {
 
     @Input() srcs?: string[];
     @Input() height: string;
     @Input() width: string;
     @Input() autoPlay?: boolean | { interval?: number };
+    isAutoPlaySubject: BehaviorSubject<boolean>;
+    private isAutoPlaySubscription: Subscription;
+
+    private autoPlayIntervalId?: number;
     safeUrls: SafeUrl[];
     index: number;
 
@@ -25,11 +30,33 @@ export class MediaViewerComponent implements OnInit {
         this.index = 0;
         if (this.srcs) this.safeUrls = this.srcs.map((url) => this.sanitization.bypassSecurityTrustResourceUrl(url));
 
+        this.isAutoPlaySubject = new BehaviorSubject<boolean>(!!this.autoPlay);
+        this.isAutoPlaySubscription = this.isAutoPlaySubject.asObservable().subscribe({
+            next: (isAutoPlay) => {
+                this.logger.log(`Auto play is ${isAutoPlay}`);
+                if (isAutoPlay) this.setAutoPlayInterval();
+                else if(this.autoPlayIntervalId) window.clearInterval(this.autoPlayIntervalId);
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.isAutoPlaySubscription.unsubscribe();
+    }
+
+    setAutoPlayInterval() {
         const defaultInterval = 3000;
         const interval = this.autoPlay && hasInterval(this.autoPlay) ? this.autoPlay.interval : defaultInterval;
-        if (this.autoPlay) setInterval(() => {
+        this.autoPlayIntervalId = window.setInterval(() => {
             this.index < this.safeUrls.length - 1 ? this.index++ : this.index = 0;
         }, interval);
+    }
+
+    onClickAutoPlay(event: Event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.isAutoPlaySubject.next(!this.isAutoPlaySubject.value);
     }
 
     onClickNext() {
