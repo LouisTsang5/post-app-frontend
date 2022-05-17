e8297d97e595a172454cecd4583d154d8700135a
louistsang <louis.tsang@ricoh.com.hk>
2022-05-17 15:45:44 +0800

    added theme toggle to top util bar

--- src/app/components/top-utility-bar/top-utility-bar.component.ts
+++ src/app/components/top-utility-bar/top-utility-bar.component.ts
@@ -1,36 +1,42 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Theme, ThemeService } from 'src/app/_services/theme.service';
 import { User } from '../../_models/user';
 import { AuthenticationService } from '../../_services/authentication.service';
 
 @Component({
-  selector: 'app-top-utility-bar',
-  templateUrl: './top-utility-bar.component.html',
-  styleUrls: ['./top-utility-bar.component.scss']
+    selector: 'app-top-utility-bar',
+    templateUrl: './top-utility-bar.component.html',
+    styleUrls: ['./top-utility-bar.component.scss']
 })
 export class TopUtilityBarComponent implements OnInit, OnDestroy {
 
-  currentUser: User;
-  currentUserSubscription: Subscription;
+    currentUser: User;
+    currentUserSubscription: Subscription;
 
-  constructor(
-    private authenticationService: AuthenticationService
-  ) { }
+    constructor(
+        private authenticationService: AuthenticationService,
+        private themeService: ThemeService,
+    ) { }
 
-  ngOnInit(): void {
-    this.currentUserSubscription = this.authenticationService.currentUserObservable.subscribe({
-      next: (user?) => {
-        if (user)
-          this.currentUser = user;
-      }
-    });
-  }
+    ngOnInit(): void {
+        this.currentUserSubscription = this.authenticationService.currentUserObservable.subscribe({
+            next: (user?) => {
+                if (user)
+                this.currentUser = user;
+            }
+        });
+    }
 
-  ngOnDestroy(): void {
-      this.currentUserSubscription.unsubscribe();
-  }
+    ngOnDestroy(): void {
+        this.currentUserSubscription.unsubscribe();
+    }
 
-  public onLogout() {
-    this.authenticationService.logout();
-  }
+    public onLogout() {
+        this.authenticationService.logout();
+    }
+
+    onClickTheme() {
+        this.themeService.theme = this.themeService.theme === Theme.Light ? Theme.Dark : Theme.Light;
+    }
 }
