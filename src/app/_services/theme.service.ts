c043f2347f23c99bed52110a7c47b9bb3b672fae
louistsang <louis.tsang@ricoh.com.hk>
2022-05-17 15:45:18 +0800

    added prototype theme function to theme service

--- src/app/_services/theme.service.ts
+++ src/app/_services/theme.service.ts
@@ -18,19 +18,45 @@ export class ThemeService implements OnDestroy {
         });
     }
 
-    set Theme(theme: Theme) {
+    set theme(theme: Theme) {
         this.themeSubject.next(theme);
     }
 
-    setTheme(theme: Theme) {
-        const styles: { name: string, value: string }[] = [
-            { name: 'primary-color', value: 'rgb(59 130 246)' }, //blue-500
-            { name: 'secondary-color', value: 'rgb(191 219 254' }, //blue-200
-            { name: 'background-color', value: 'white' },
-        ];
-        styles.forEach(({ name, value }) => {
-            document.documentElement.style.setProperty(`--${name}`, value);
-        });
+    get theme() {
+        return this.themeSubject.value;
+    }
+
+    private setTheme(theme: Theme) {
+        const styleLight = {
+            'primary-color': 'rgb(59 130 246)', //blue-500
+            'primary-color-inverse': 'white',
+            'primary-text-color': 'var(--primary-color-inverse)',
+            'primary-text-color-inverse': 'var(--primary-color)',
+            'secondary-color': 'rgb(191 219 254)', //blue-200
+            'secondary-text-color': 'black',
+            'background-color': 'white',
+            'background-color-light': 'rgb(243 244 246)', //gray-100
+            'background-highlight-color': 'rgb(229 231 235)', //gray-200
+            'background-text-color': 'black',
+        };
+        const styleDark = {
+            'primary-color': 'rgb(79 70 229)', //indigo-600
+            'primary-color-inverse': 'white',
+            'primary-text-color': 'var(--primary-color-inverse)',
+            'primary-text-color-inverse': 'var(--primary-color)',
+            'secondary-color': 'rgb(191 219 254)', //blue-200
+            'secondary-text-color': 'white',
+            'background-color': 'rgb(15 23 42)', //slate-900
+            'background-color-light': 'rgb(71 85 105)', //slate-600
+            'background-highlight-color': 'rgb(100 116 139)', //slate-500
+            'background-text-color': 'white',
+        };
+
+        const style = theme === Theme.Dark ? styleDark : styleLight;
+
+        for (let [key, value] of Object.entries(style)) {
+            document.documentElement.style.setProperty(`--${key}`, value);
+        }
     }
 
     ngOnDestroy(): void {
